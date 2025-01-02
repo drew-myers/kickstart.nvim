ecc3b2df56ae10e957d6cc05e9ec5a8c28f82f4e
Drew Myers <drew.myers@close.com>
2025-01-02 11:24:19 -0500

    add support for local override

--- init.lua
+++ init.lua
@@ -134,6 +134,22 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
+-- [[ Local override capability, originally devised to allow local overriding of colorscheme
+-- different machines.
+-- EXTENSION POINT: add functions to local and call function to override non plugin
+-- settings like above keymaps and etc
+local overrides_enabled, local_overrides = pcall(require, 'local')
+
+--  Used in combination with local overrides above within lazy config to
+--  load optionally overriden local values
+local function override_or_default(enabled, override, default)
+  if not enabled or override == nil then
+    return default
+  else
+    return override
+  end
+end
+
 -- [[ Configure and install plugins ]]
 require('lazy').setup({
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
@@ -710,32 +726,7 @@ require('lazy').setup({
     end,
   },
 
-  --[[ { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  }, ]]
-  --[[ {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    priority = 1000,
-    init = function()
-      vim.cmd.colorscheme 'rose-pine'
-    end,
-  }, ]]
-  {
+  override_or_default(overrides_enabled, local_overrides.colorscheme, {
     'neanias/everforest-nvim',
     version = false,
     lazy = false,
@@ -743,7 +734,7 @@ require('lazy').setup({
     init = function()
       vim.cmd.colorscheme 'everforest'
     end,
-  },
+  }),
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
