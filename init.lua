8a8c8ecb20135727ecd3afc9c949ba1778e76a14
Drew Myers <drew.myers@close.com>
2025-01-03 20:08:28 -0500

    trial using arena for buffer picker

--- init.lua
+++ init.lua
@@ -333,7 +333,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      --vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
@@ -1085,6 +1085,16 @@ require('lazy').setup({
     keys = {
       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
+  },
+
+  {
+    'dzfrias/arena.nvim',
+    event = 'BufWinEnter',
+    config = function()
+      local arena = require 'arena'
+      arena.setup()
+      vim.keymap.set('n', '<leader><leader>', arena.toggle, { desc = '[ ] Arena buffer picker' })
+    end,
   },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
